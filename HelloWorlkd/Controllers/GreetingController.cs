d3b483ec94b131e4a2aad734d89a516d75cf24eb
StoryOff <sotic.roma@gmail.com>
2025-02-12 02:37:25 +0800

    Добавьте файлы проекта.

---
+++ HelloWorlkd/Controllers/GreetingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorlkd.Controllers
+{
+    public class GreetingController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("NameYourself", "Greeting");
+        }
+        public IActionResult NameYourself()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Greet(string name = "Guest")
+        {
+            ViewBag.Message = $"Greetings, {name}!";
+            return View();
+        }
+    }
+}
